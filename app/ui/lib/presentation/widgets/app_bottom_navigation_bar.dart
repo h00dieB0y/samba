c8d6589bf90d8ce3387a98a8a0bfb2fb29cc2081
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-15 13:04:01 +0100

    feat: add AppBottomNavigationBar widget for navigation

---
+++ app/ui/lib/presentation/widgets/app_bottom_navigation_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppBottomNavigationBar extends StatelessWidget {
+  const AppBottomNavigationBar(
+      {Key? key, required this.onTap, required this.currentIndex})
+      : super(key: key);
+
+  final Function(int) onTap;
+  final int currentIndex;
+
+  @override
+  Widget build(BuildContext context) => BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: 0, // Set the current index as needed
+        onTap: (index) {
+          // Handle navigation on tap
+        },
+        backgroundColor: Colors.white, // Set the background color
+        selectedItemColor: Colors.blue, // Set the color of the selected item
+        unselectedItemColor:
+            Colors.grey, // Set the color of the unselected items
+      );
+}
