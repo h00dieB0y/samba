2bed40b2fa4a5796f551388cb82f97f1a255998f
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-17 03:02:37 +0100

    feat: add Product entity with attributes and methods

---
+++ app/api/src/main/java/com/somba/api/core/entities/Product.java
@@ -0,0 +1,64 @@
+package com.somba.api.core.entities;
+
+import java.util.UUID;
+
+public class Product {
+  
+  private UUID id;
+
+  private String name;
+
+  private String description;
+
+  private String brand;
+
+  /*
+   * Price in cents
+   */
+  private int price;
+  private int stock;
+
+  public Product() {
+  }
+
+  public Product(UUID id, String name, String description, int price, int stock) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  public UUID getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getDescription(){
+    return this.description;
+  }
+
+  public int getPrice() {
+    return this.price;
+  }
+
+  public int getStock() {
+    return this.stock;
+  }
+
+  @Override
+  public String toString() {
+    return "Product{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", description='" + description + '\'' +
+        ", brand='" + brand + '\'' +
+        ", price=" + price +
+        ", stock=" + stock +
+        '}';
+  }
+
+}
