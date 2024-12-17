e34f1ed7decf7104eb9ee784b35b810ab6e581bb
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-17 14:22:05 +0100

    refactor: rename ProductRepository interface to reflect its role in core ports

--- app/api/src/main/java/com/somba/api/core/ports/ProductRepository.java
+++ app/api/src/main/java/com/somba/api/core/ports/ProductRepository.java
@@ -1,12 +1,9 @@
-package com.somba.api.adapters.repositories;
+package com.somba.api.core.ports;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.somba.api.core.entities.Product;
 
-@Repository
 public interface ProductRepository {
 
   List<Product> getProducts(int page, int size);
