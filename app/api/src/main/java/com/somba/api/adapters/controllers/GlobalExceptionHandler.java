1cf8ee17eb6342acedf0fe095338ce834f766186
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-18 13:58:39 +0100

    refactor: update GlobalExceptionHandler to use ServletUriComponentsBuilder for request URI

--- app/api/src/main/java/com/somba/api/adapters/controllers/GlobalExceptionHandler.java
+++ app/api/src/main/java/com/somba/api/adapters/controllers/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.somba.api.adapters.controllers;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import com.somba.api.adapters.presenters.ErrorDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,13 +17,13 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public ErrorDetails handleAllExceptions(Exception ex, HttpServletRequest request) {
+  public ErrorDetails handleAllExceptions(Exception ex) {
     logger.error("An unexpected error occurred", ex);
     return new ErrorDetails(
       HttpStatus.INTERNAL_SERVER_ERROR.value(),
       "Internal Server Error",
       "An unexpected error occurred. Please try again later.",
-      request.getRequestURI()
+      ServletUriComponentsBuilder.fromCurrentRequest().toUriString()
     );
   }
 }
