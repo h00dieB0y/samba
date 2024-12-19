32959579d28061fa6a0745d7f02604bd3434a0ee
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-19 02:46:42 +0100

    refactor: add validation to listProducts parameters for improved input handling

--- app/api/src/main/java/com/somba/api/adapter/controllers/ProductController.java
+++ app/api/src/main/java/com/somba/api/adapter/controllers/ProductController.java
@@ -1,7 +1,6 @@
 package com.somba.api.adapter.controllers;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import com.somba.api.adapter.mappers.ProductMapper;
 import com.somba.api.adapter.presenters.ProductView;
@@ -12,9 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@Validated
 public class ProductController {
 
   private final ListProductsUseCase listProductsUseCase;
@@ -25,10 +28,11 @@ public class ProductController {
     this.productMapper = productMapper;
   }
 
-  @GetMapping(produces = "application/json")
+  @GetMapping(produces = { "application/json" })
   public Response<List<ProductView>> listProducts(
-      @RequestParam(defaultValue = "0") int page,
-      @RequestParam(defaultValue = "10") int size) {
+      @RequestParam(defaultValue = "0") @Min(value = 0, message = "Page number must be greater than or equal to 0") int page,
+      @RequestParam(defaultValue = "10") @Min(value = 1, message = "Page size must be greater than or equal to 1") int size
+  ) {
     return new Response<>(
       200,
       "Successfully retrieved the list of products",
