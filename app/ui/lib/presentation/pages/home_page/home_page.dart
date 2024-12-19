c2ebe805b7d91f21e88efed7d1bd3bcd8281e5cd
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-19 23:09:11 +0100

    feat(ui): refactor main app structure to include repository and use case providers

--- app/ui/lib/presentation/pages/home_page/home_page.dart
+++ app/ui/lib/presentation/pages/home_page/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ui/domain/usecases/get_products_use_case.dart';
+import 'package:ui/presentation/cubits/products/products_cubit.dart';
 import 'package:ui/presentation/pages/home_page/widgets/home_header.dart';
 import 'package:ui/presentation/pages/home_page/widgets/product_grid.dart';
 import 'package:ui/presentation/widgets/app_bottom_navigation_bar.dart';
@@ -9,17 +12,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const SafeArea(
-        child: Column(
-          children: [
-            HomeHeader(),
-            ProductGrid(),
-          ],
+      body: RepositoryProvider(
+        create: (context) => GetProductsUseCase(context.read()),
+
+        child: BlocProvider(
+          create: (context) => ProductsCubit(context.read<GetProductsUseCase>())..getProductList(),
+          child: const SafeArea(
+            child: Column(
+              children: [
+                HomeHeader(),
+                Expanded(child: ProductGrid()),
+              ],
+            ),
+          ),
         ),
       ),
-      bottomNavigationBar: AppBottomNavigationBar(onTap: (index) {
-        // Handle navigation on tap
-      }, currentIndex: 0),
+      bottomNavigationBar: AppBottomNavigationBar(
+        onTap: (index) {
+          // Handle navigation on tap
+        },
+        currentIndex: 0,
+      ),
     );
   }
 }
