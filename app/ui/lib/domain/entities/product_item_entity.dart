f6e32bfed4e696f2670aa00b85172cca1115b05c
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-21 00:30:36 +0100

    feat(models): extend ProductItemEntity with Equatable for value comparison

--- app/ui/lib/domain/entities/product_item_entity.dart
+++ app/ui/lib/domain/entities/product_item_entity.dart
@@ -1,14 +1,18 @@
+import 'package:equatable/equatable.dart';
 
-class ProductItemEntity {
+class ProductItemEntity extends Equatable {
   final String id;
   final String name;
   final String brand;
   final String price;
 
-  ProductItemEntity({
+  const ProductItemEntity({
     required this.id,
     required this.name,
     required this.brand,
     required this.price,
   });
+
+  @override
+  List<Object?> get props => [id, name, brand, price];
 }
