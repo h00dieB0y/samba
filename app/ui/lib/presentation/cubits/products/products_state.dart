5766685adaf30d23a91f88d037018725700a856b
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-21 17:06:02 +0100

    feat(cubit): implement pagination and loading states for product fetching

--- app/ui/lib/presentation/cubits/products/products_state.dart
+++ app/ui/lib/presentation/cubits/products/products_state.dart
@@ -8,10 +8,13 @@ abstract class ProductsState extends Equatable {
   List<Object> get props => [];
 }
 
+/// Initial uninitialized state
 class ProductsInitial extends ProductsState {}
 
+/// State when the first batch of data is loading
 class ProductsLoading extends ProductsState {}
 
+/// State when the first (or refreshed) batch of data has loaded
 class ProductsLoaded extends ProductsState {
   final List<ProductItemEntity> products;
 
@@ -21,6 +24,17 @@ class ProductsLoaded extends ProductsState {
   List<Object> get props => [products];
 }
 
+/// State when loading additional (paginated) data
+class ProductsLoadingMore extends ProductsState {
+  final List<ProductItemEntity> products;
+
+  const ProductsLoadingMore(this.products);
+
+  @override
+  List<Object> get props => [products];
+}
+
+/// Error state
 class ProductsError extends ProductsState {
   final String message;
 
