228671f1a35eb323148db94c42e958a3827cda54
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-22 14:49:12 +0100

    refactor: Enhance error message construction in InvalidPaginationParameterException for clarity

--- app/api/src/main/java/com/somba/api/core/exceptions/InvalidPaginationParameterException.java
+++ app/api/src/main/java/com/somba/api/core/exceptions/InvalidPaginationParameterException.java
@@ -2,8 +2,20 @@ package com.somba.api.core.exceptions;
 
 public class InvalidPaginationParameterException extends RuntimeException {
     public InvalidPaginationParameterException(int page, int size) {
-        super("Invalid pagination parameters provided: " +
-                "page=" + page + " (must be >= 0) and " +
-                "size=" + size + " (must be > 0).");
+        super(constructMessage(page, size));
+    }
+
+    private static String constructMessage(int page, int size) {
+        String message = "Invalid pagination parameters provided: ";
+        String pageMessage = "page=" + page + " (must be >= 0)";
+        String sizeMessage = "size=" + size + " (must be > 0)";
+
+        if (page < 0 && size < 1) {
+            return message + pageMessage + " and " + sizeMessage + ".";
+        } else if (page < 0) {
+            return message + pageMessage + ".";
+        } else {
+            return message + sizeMessage + ".";
+        }
     }
 }
