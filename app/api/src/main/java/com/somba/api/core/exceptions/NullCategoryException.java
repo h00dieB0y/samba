5ddf7461aba5e4b944c066814814bbe59e9574a3
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-22 15:16:30 +0100

    fix: Simplify NullCategoryException by removing redundant constructors

--- app/api/src/main/java/com/somba/api/core/exceptions/NullCategoryException.java
+++ app/api/src/main/java/com/somba/api/core/exceptions/NullCategoryException.java
@@ -1,14 +1,6 @@
 package com.somba.api.core.exceptions;
 
 public class NullCategoryException extends RuntimeException {
-    public NullCategoryException(String message) {
-        super(message);
-    }
-
-    public NullCategoryException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public NullCategoryException() {
         super("Category cannot be null");
     }
