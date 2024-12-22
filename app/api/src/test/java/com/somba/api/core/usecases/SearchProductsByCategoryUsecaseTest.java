d4ec580cd91508c3e71373a55eef0af9e102f403
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-22 01:05:19 +0100

    refactor: Remove unused import in SearchProductsByCategoryUsecaseTest

--- app/api/src/test/java/com/somba/api/core/usecases/SearchProductsByCategoryUsecaseTest.java
+++ app/api/src/test/java/com/somba/api/core/usecases/SearchProductsByCategoryUsecaseTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
