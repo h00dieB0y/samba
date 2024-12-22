d12cd99f2c2232e5039ab412d9a53dc5baacfff3
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-22 01:46:58 +0100

    refactor: Improve error message formatting in InvalidPaginationParameterException

--- app/api/src/main/java/com/somba/api/core/exceptions/InvalidPaginationParameterException.java
+++ app/api/src/main/java/com/somba/api/core/exceptions/InvalidPaginationParameterException.java
@@ -3,7 +3,7 @@ package com.somba.api.core.exceptions;
 public class InvalidPaginationParameterException extends RuntimeException {
     public InvalidPaginationParameterException(int page, int size) {
         super("Invalid pagination parameters provided: " +
-                "page=" + page + "(must be greater or equal to 0) and " +
-                "size=" + size + "(must be greater than 0)");
+                "page=" + page + " (must be >= 0) and " +
+                "size=" + size + " (must be > 0).");
     }
 }
