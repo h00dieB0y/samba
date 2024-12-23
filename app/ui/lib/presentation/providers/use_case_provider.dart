2793054c2ed96c8787803776f2ecce3e510e9909
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-23 02:08:12 +0100

    feat(products): enhance ProductsNotifier to support fetching products by category

--- app/ui/lib/presentation/providers/use_case_provider.dart
+++ app/ui/lib/presentation/providers/use_case_provider.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ui/domain/usecases/get_products_by_category_use_case.dart';
 import 'package:ui/domain/usecases/get_products_use_case.dart';
 import 'product_provider.dart';
 
 // Provides the Use Case for Getting Products
 final getProductsUseCaseProvider = Provider(
   (ref) => GetProductsUseCase(ref.read(productRepositoryProvider)),
+);
+
+final getProductsByCategoryUseCaseProvider = Provider(
+  (ref) => GetProductsByCategoryUseCase(ref.read(productRepositoryProvider)),
 );
