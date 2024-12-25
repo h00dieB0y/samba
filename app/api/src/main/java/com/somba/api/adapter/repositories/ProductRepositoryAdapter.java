df757805ffeb15043db13ead00f81fde644f0f0a
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-25 16:17:59 +0100

    Refactor(api): remove ElasticSearchProductRepositoryAdapter and rename MdbProductRepositoryAdapter to ProductRepositoryAdapter

--- app/api/src/main/java/com/somba/api/adapter/repositories/ProductRepositoryAdapter.java
+++ app/api/src/main/java/com/somba/api/adapter/repositories/ProductRepositoryAdapter.java
@@ -7,18 +7,19 @@ import com.somba.api.adapter.mappers.ProductMapper;
 import com.somba.api.core.entities.Product;
 import com.somba.api.core.enums.Category;
 import com.somba.api.core.ports.ProductRepository;
+import com.somba.api.core.ports.ProductSearchRepository;
 import com.somba.api.infrastructure.persistence.MdbProductRepository;
 
 import org.springframework.stereotype.Component;
 
 @Component
-public class MdbProductRepositoryAdapter implements ProductRepository {
+public class ProductRepositoryAdapter implements ProductRepository, ProductSearchRepository {
 
   private final MdbProductRepository mdbProductRepository;
 
   private final ProductMapper productMapper;
 
-  public MdbProductRepositoryAdapter(MdbProductRepository mdbProductRepository, ProductMapper productMapper) {
+  public ProductRepositoryAdapter(MdbProductRepository mdbProductRepository, ProductMapper productMapper) {
     this.mdbProductRepository = mdbProductRepository;
     this.productMapper = productMapper;
   }
@@ -53,5 +54,17 @@ public class MdbProductRepositoryAdapter implements ProductRepository {
       .findByCategory(category.name(), PageRequest.of(page, size))
       .map(productMapper::toDomain)
       .toList();
+  }
+
+  @Override
+  public void index(Product product) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'index'");
+  }
+
+  @Override
+  public List<Product> search(String query) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'search'");
   }
 }
