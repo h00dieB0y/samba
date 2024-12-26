ff953e8f85876845341de46867a99acceb8f33ab
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-26 13:17:09 +0100

    Feat(api): add comments to ElasticsearchProductSearchRepository for clarity on query behavior

--- app/api/src/main/java/com/somba/api/infrastructure/search/ElasticsearchProductSearchRepository.java
+++ app/api/src/main/java/com/somba/api/infrastructure/search/ElasticsearchProductSearchRepository.java
@@ -5,5 +5,7 @@ import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ElasticsearchProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
+  // Exact match and null values match all
+  // With Pageable
   List<ProductDocument> findByNameOrDescriptionOrBrand(String name, String description, String brand);
 }
