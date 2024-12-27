94fecf01b65f635a6366d1ddde30f6c7d5f0e064
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-28 00:50:33 +0100

    Feat(api): add method to Product entity for adding reviews

--- app/api/src/main/java/com/somba/api/core/entities/Product.java
+++ app/api/src/main/java/com/somba/api/core/entities/Product.java
@@ -1,5 +1,6 @@
 package com.somba.api.core.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,10 +18,14 @@ public record Product(
 ) {
 
   public Product(UUID id, String name, String description, String brand, int price, int stock, Category category) {
-    this(id, name, description, brand, price, stock, category, List.of());
+    this(id, name, description, brand, price, stock, category, new ArrayList<>());
   }
 
   public Product(String name, String description, String brand, int price, int stock, Category category) {
     this(UUID.randomUUID(), name, description, brand, price, stock, category);
+  }
+
+  public void addReview(Review review) {
+    reviews.add(review);
   }
 }
