f70e9c0c8c31dff1743822dfe9b6e94bc3a498f4
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-28 00:51:32 +0100

    Feat(api): add getProductById method to ProductRepository for retrieving products by UUID

--- app/api/src/main/java/com/somba/api/core/ports/ProductRepository.java
+++ app/api/src/main/java/com/somba/api/core/ports/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.somba.api.core.ports;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import com.somba.api.core.entities.Product;
 import com.somba.api.core.enums.Category;
@@ -9,7 +11,11 @@ public interface ProductRepository {
 
   List<Product> getProducts(int page, int size);
 
+  Optional<Product> getProductById(UUID id);
+
   List<Product> getProductsByCategory(Category category, int page, int size);
+
+  Product save(Product product);
 
   void saveAll(List<Product> products);
 
