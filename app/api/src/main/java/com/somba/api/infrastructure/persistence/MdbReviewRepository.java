bf1795ae47d7e8a80fb6bf0c4ab6ef862aa1d324
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-28 15:07:19 +0100

    Feat(api): add averageRating method to MdbReviewRepository for calculating product rating

--- app/api/src/main/java/com/somba/api/infrastructure/persistence/MdbReviewRepository.java
+++ app/api/src/main/java/com/somba/api/infrastructure/persistence/MdbReviewRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.somba.api.infrastructure.persistence.entities.ReviewEntity;
 
 public interface MdbReviewRepository extends MongoRepository<ReviewEntity, String> {
+  double averageRating(String productId);
 }
