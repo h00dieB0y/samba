71c732c7d34adf9ae8ae0356283bca258008358f
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-28 15:22:36 +0100

    Feat(api): enhance averageRating method in MdbReviewRepository with aggregation pipeline for improved performance

--- app/api/src/main/java/com/somba/api/infrastructure/persistence/MdbReviewRepository.java
+++ app/api/src/main/java/com/somba/api/infrastructure/persistence/MdbReviewRepository.java
@@ -1,9 +1,16 @@
 package com.somba.api.infrastructure.persistence;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.somba.api.infrastructure.persistence.entities.ReviewEntity;
 
 public interface MdbReviewRepository extends MongoRepository<ReviewEntity, String> {
-  double averageRating(String productId);
+  @Aggregation(
+    pipeline = {
+      "{ $match: { productId: ?0 } }",
+      "{ $group: { _id: null, averageRating: { $avg: '$rating' } } }"
+    }
+  )
+  Double averageRating(String productId);
 }
